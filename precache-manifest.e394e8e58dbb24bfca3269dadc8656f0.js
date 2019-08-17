ea541e732ac99d654caa69c0e2b1f3fd17da27a5
Tami Williams <tami@asktami.com>
2019-08-17 12:34:32 -0400

    Updates

---
+++ precache-manifest.e394e8e58dbb24bfca3269dadc8656f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13ba0407c318d5325938e58ea057718",
+    "url": "/react-laptop_customizer/index.html"
+  },
+  {
+    "revision": "c642083e514c0288256d",
+    "url": "/react-laptop_customizer/static/css/main.a3270a3d.chunk.css"
+  },
+  {
+    "revision": "48d359e5ff1ff505ecff",
+    "url": "/react-laptop_customizer/static/js/2.ad9d7570.chunk.js"
+  },
+  {
+    "revision": "c642083e514c0288256d",
+    "url": "/react-laptop_customizer/static/js/main.82c6ffdc.chunk.js"
+  },
+  {
+    "revision": "ee127fd20a8bf902a42a",
+    "url": "/react-laptop_customizer/static/js/runtime~main.66eb7f19.js"
+  }
+]);
